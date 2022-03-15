5827a4aea30707c2e916be635c2e55a8750448de
Sangbin <mosangbin94@gmail.com>
2022-03-15 15:31:38 +0900

    css작성

--- dream-coding-react/youtube/src/components/Video_list/Video_list.jsx
+++ dream-coding-react/youtube/src/components/Video_list/Video_list.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import VideoItem from "../Video_item/Video-item";
+import VideoItem from "../Video_item/Video_item";
+import styles from "./Video_list.module.css";
 
 const VideoList = (props) => (
-  <ul>
+  <ul className={styles.videos}>
     {props.videos.map((video) => (
       <VideoItem key={video.id} video={video} />
     ))}
