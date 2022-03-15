3f3496bd3380695038cb8a369ed6061a222d019e
Sangbin <mosangbin94@gmail.com>
2022-03-15 16:51:26 +0900

    SearchHeader css작성

--- dream-coding-react/youtube/src/components/Search_header/Search_header.jsx
+++ dream-coding-react/youtube/src/components/Search_header/Search_header.jsx
@@ -3,12 +3,14 @@ import styles from "./Search_header.module.css";
 
 const Search_header = (props) => {
   return (
-    <header>
-      <img src="/images/logo.png" alt="logo" />
-      <h1>Youtube</h1>
-      <input type="text" placeholder="Search" />
-      <button type="submit">
-          <img src="/images/search.png" alt="search" />
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img className={styles.img} src="/images/logo.png" alt="logo" />
+        <h1 className={styles.title}>Youtube</h1>
+      </div>
+      <input className={styles.input} type="text" placeholder="Search" />
+      <button className={styles.button} type="submit">
+        <img className={styles.buttonImg} src="/images/search.png" alt="search" />
       </button>
     </header>
   );
