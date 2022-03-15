f67bfb25653b56e568b2df764bb97ecc16f49942
Sangbin <mosangbin94@gmail.com>
2022-03-15 13:26:22 +0900

    유튜브API 가져오기

--- dream-coding-react/youtube/src/app.jsx
+++ dream-coding-react/youtube/src/app.jsx
@@ -1,8 +1,24 @@
+import React, { useEffect, useState } from 'react';
 import './app.css';
 
 function App() {
+  const [video, setVideos] = useState([]);
+
+
+  useEffect(()=> {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDhEeHaM-lrpgIYTkseTk-eiB9kp9X4-qg", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.item))
+      .catch(error => console.log('error', error));
+  }, []);
+
   return (
-    <h1>Hello :)</h1>
+    <div>{video}</div>
   );
 }
 
