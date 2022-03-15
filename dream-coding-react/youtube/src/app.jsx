cad6ab75377aaa4d3298a4579e62e9eec004062f
Sangbin <mosangbin94@gmail.com>
2022-03-15 14:13:55 +0900

    VideoList, VideoItem 컴포넌트 생성

--- dream-coding-react/youtube/src/app.jsx
+++ dream-coding-react/youtube/src/app.jsx
@@ -1,25 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import './app.css';
+import React, { useEffect, useState } from "react";
+import "./app.css";
+import VideoList from "./components/Video_list/Video_list";
 
 function App() {
-  const [video, setVideos] = useState([]);
+  const [videos, setVideos] = useState([]);
 
-
-  useEffect(()=> {
+  useEffect(() => {
     const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
+      method: "GET",
+      redirect: "follow",
     };
-    
-    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDhEeHaM-lrpgIYTkseTk-eiB9kp9X4-qg", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.item))
-      .catch(error => console.log('error', error));
+
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDhEeHaM-lrpgIYTkseTk-eiB9kp9X4-qg",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
   }, []);
 
-  return (
-    <div>{video}</div>
-  );
+  return <VideoList videos={videos} />;
 }
 
 export default App;
--- dream-coding-react/youtube/src/components/Video_list/Video_list.jsx
+++ dream-coding-react/youtube/src/components/Video_list/Video_list.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import VideoItem from "../Video_item/Video-item";
 
-const VideoList = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+const VideoList = (props) => (
+  <ul>
+    {props.videos.map((video) => (
+      <VideoItem key={video.id} video={video} />
+    ))}
+  </ul>
+);
 
 export default VideoList;
