3357c2d5fc831e04864e57c72d3dae63d6e56fbb
Sangbin <mosangbin94@gmail.com>
2022-03-17 17:53:00 +0900

    Video 상세페이지

--- dream-coding-react/youtube/src/components/Video_detail/Video_detail.jsx
+++ dream-coding-react/youtube/src/components/Video_detail/Video_detail.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import styles from "./Video_detail.module.css";
 
 const Video_detail = ({ video }) => {
-  return <div>{video.snippet.title}</div>;
+  return (
+    <section className={styles.detail}>
+      <iframe
+        className={styles.video}
+        type="text/html"
+        width="100%"
+        height="500px"
+        src={`https://www.youtube.com/embed/${video.id}`}
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+      <h2>{video.snippet.title}</h2>
+      <h3>{video.snippet.channelTitle}</h3>
+      <pre className={styles.description}>{video.snippet.description}</pre>
+    </section>
+  );
 };
 
 export default Video_detail;
