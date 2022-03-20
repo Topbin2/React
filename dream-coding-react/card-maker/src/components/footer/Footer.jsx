23b6955a4d168e5bcb5d1de0f2497085b7570a1a
Sangbin <mosangbin94@gmail.com>
2022-03-20 19:58:25 +0900

    footer 컴포넌트

--- dream-coding-react/card-maker/src/components/footer/Footer.jsx
+++ dream-coding-react/card-maker/src/components/footer/Footer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from './footer.module.css';
 
-const Footer = (props) => {
-  return <h1>Footer</h1>;
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.title}>Code your dream</p>
+    </footer>
+  );
 };
 
 export default Footer;
