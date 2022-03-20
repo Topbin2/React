41cb618b979dc64f99f3a42773f364513ed8200c
Sangbin <mosangbin94@gmail.com>
2022-03-20 20:05:43 +0900

    login 컴포넌트 마크업

--- dream-coding-react/card-maker/src/components/header/Header.jsx
+++ dream-coding-react/card-maker/src/components/header/Header.jsx
@@ -4,7 +4,7 @@ import styles from "./header.module.css";
 const Header = ({ onLogout }) => {
   return (
     <header className={styles.header}>
-      {true && (
+      {onLogout && (
         <button className={styles.logout} onClick={onLogout}>
           Logout
         </button>
