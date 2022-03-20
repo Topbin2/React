bb8350e131553489261ca59fdd4e0cfc6eba745c
Sangbin <mosangbin94@gmail.com>
2022-03-20 19:53:50 +0900

    header 컴포넌트 css 작성

--- dream-coding-react/card-maker/src/components/header/Header.jsx
+++ dream-coding-react/card-maker/src/components/header/Header.jsx
@@ -4,7 +4,7 @@ import styles from "./header.module.css";
 const Header = ({ onLogout }) => {
   return (
     <header className={styles.header}>
-      {onLogout && (
+      {true && (
         <button className={styles.logout} onClick={onLogout}>
           Logout
         </button>
