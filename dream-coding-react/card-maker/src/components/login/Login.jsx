2d644cdd624612172143eb7f146e2b14e4bbdb30
Sangbin <mosangbin94@gmail.com>
2022-03-21 19:07:02 +0900

    Logout 기능 구현

--- dream-coding-react/card-maker/src/components/login/Login.jsx
+++ dream-coding-react/card-maker/src/components/login/Login.jsx
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../header/header";
 import Footer from "../footer/footer";
 import styles from "./login.module.css";
 import { useNavigate } from "react-router";
 
 const Login = ({ authService }) => {
-  const navigate = useNavigate();
+  let navigate = useNavigate();
   const goToMaker = userId => {
-    navigate({
-      pathname: "/maker",
-      state: { id: userId },
-    });
+    navigate('/maker', { state: {id: userId }});
+    console.log(userId);
   };
+
 
   const onLogin = (event) => {
     authService //
       .login(event.currentTarget.textContent)
       .then(data => goToMaker(data.user.uid));
   };
+
+  useEffect(()=> {
+    authService.onAuthChange(user => {
+      user && goToMaker(user.uid);
+    })
+  });
 
   return (
     <section className={styles.login}>
--- dream-coding-react/card-maker/src/components/maker/maker.jsx
+++ dream-coding-react/card-maker/src/components/maker/maker.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import Header from '../header/header';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router";
+import Footer from "../footer/footer";
+import Header from "../header/header";
+import styles from "./maker.module.css";
 
-const Maker = () => {
-    return (
-        <Header />
-    );
+const Maker = ({ authService }) => {
+    const navigate = useNavigate();
+
+    const onLogout = ()=> {
+        authService.logout();
+    };
+
+    useEffect(()=> {
+        authService.onAuthChange(user => {
+            if(!user) {
+                navigate('/');
+            }
+        })
+    });
+
+  return (
+    <section className={styles.maker}>
+      <Header onLogout={onLogout} />
+      <Footer />
+    </section>
+  );
 };
 
 export default Maker;
