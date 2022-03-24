daec38a2c227e9887e9b6fa27c16940a5f75fe32
Sangbin <mosangbin94@gmail.com>
2022-03-24 13:35:09 +0900

    add CardAddForm component

--- dream-coding-react/card-maker/src/components/editor/editor.jsx
+++ dream-coding-react/card-maker/src/components/editor/editor.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import AddForm from "../addForm/addForm";
 import Form from "../form/form";
 import styles from "./editor.module.css";
 
-const Editor = ({ cards }) => {
+const Editor = ({ cards, addCard }) => {
   return (
     <section className={styles.editor}>
       <h1 className={styles.title}>Card Maker</h1>
-      {cards.map((card) => (<Form card={card} />))}
+      {cards.map((card) => (<Form key={card.id} card={card} />))}
+      <AddForm onAdd={addCard}/>
     </section>
   );
 };
--- dream-coding-react/card-maker/src/components/maker/maker.jsx
+++ dream-coding-react/card-maker/src/components/maker/maker.jsx
@@ -56,11 +56,16 @@ const Maker = ({ authService }) => {
         })
     });
 
+    const addCard = (card)=> {
+      const updated = [...cards, card];
+      setCards(updated);
+    };
+
   return (
     <section className={styles.maker}>
       <Header className={styles.header} onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor cards={cards} />
+        <Editor cards={cards} addCard={addCard} />
         <Preview cards={cards} />
       </div>
       <Footer className={styles.footer} />
