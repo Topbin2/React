6ef3678e0e5bfc49b85ffc53c1628be66ddd20f0
SANGBIN <mosangbin94@gmail.com>
2022-03-30 00:45:01 +0900

    useReducer

--- Todo/src/App.js
+++ Todo/src/App.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
 import "./App.css";
-import Page from './components/Page';
 
 function App() {
-  const [isDark, setIsDark] = useState(false);
+  const [number, setNumber] = useState(0);
   return (
-    <Page isDark={isDark} setIsDark={setIsDark} />
+    <div>
+      <h1>useReducer 은행에 오신것을 환영합니다</h1>
+      <p>잔고: ?원</p>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+        step="1000"
+      />
+      <button>예금</button>
+      <button>출금</button>
+    </div>
   );
 }
 
