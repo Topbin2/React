a0ca69498ef03ccb3830d8b2ed4d77daa9003bed
SANGBIN <mosangbin94@gmail.com>
2022-03-30 16:07:49 +0900

    add button component

--- Udemy/order-app/src/components/Layout/Header.jsx
+++ Udemy/order-app/src/components/Layout/Header.jsx
@@ -1,13 +1,14 @@
 import React, { Fragment } from 'react';
 import styles from './Header.module.css';
 import mealsImage from '../../aseets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (porops) => {
     return (
         <Fragment>
             <header className={styles.header}>
                 <h1>ReactMeals</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
             <div className={styles.mainImage}>
                 <img src={mealsImage} alt="A table full of delicious food!" />
