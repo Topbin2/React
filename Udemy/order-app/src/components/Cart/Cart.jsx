f922c00dcaee919d057febb7c3859a5032b928a5
SANGBIN <mosangbin94@gmail.com>
2022-03-30 20:52:38 +0900

    add cart component

---
+++ Udemy/order-app/src/components/Cart/Cart.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./Cart.module.css";
+
+const Cart = (ptops) => {
+  const cartItems = (
+    <ul className={styles['cart-items']}>
+      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((itme) => (
+        <li>{itme.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div>
+      {cartItems}
+      <div className={styles.total}>
+          <span>Total Amount</span>
+          <span>35.62</span>
+      </div>
+      <div className={styles.actions}>
+          <button className={styles['button--alt']}>Close</button>
+          <button className={styles.button}>Order</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
