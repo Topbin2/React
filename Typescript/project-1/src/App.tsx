897cd862f100b50e330c866d4e09d16d67aa1bf2
SANGBIN <mosangbin94@gmail.com>
2022-04-06 01:50:46 +0900

    typescript

--- Typescript/project-1/src/App.tsx
+++ Typescript/project-1/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import TodoList from './components/TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+const App: React.FC = () => {
+	const todos = [{ id: 't1', text: 'Finish the course' }];
+
+	return <TodoList items={todos} />;
+};
 
 export default App;
