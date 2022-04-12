6aab225080288841d6d5d9d1675d2481f81855c0
SANGBIN <mosangbin94@gmail.com>
2022-04-12 18:55:16 +0900

    router

--- Udemy/router/src/App.js
+++ Udemy/router/src/App.js
@@ -1,26 +1,18 @@
-import { Route } from "react-router-dom";
-
-import Welcome from "./pages/Welcome";
-import Products from "./pages/Products";
-import ProductDetail from "./pages/ProductDetail";
-import MainHeader from "./components/MainHeader";
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <MainHeader />
-      <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
-        <Route path="/product-detail/:productId">
-          <ProductDetail />
-        </Route>
-      </main>
-    </div>
+    <Switch>
+      <Route path="/quotes">
+
+      </Route>
+      <Route path="/quotes/:quoteId">
+
+      </Route>
+      <Route path="/new-quote">
+        
+      </Route>
+    </Switch>
   );
 }
 
