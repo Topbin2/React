46d490c2209379db6bbf6b7844432bc06e463922
SANGBIN <mosangbin94@gmail.com>
2022-04-12 15:50:54 +0900

    redux

--- Udemy/redux-toolkit/src/store/index.js
+++ Udemy/redux-toolkit/src/store/index.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import uiSlice from "./ui-slice";
 import cartSlice from "./cart-slice";
 
-const store = configureStore({
-  reducer: { ui: uiSlice.reducer, cart: cartSlice.reducer }
-});
+const store = configureStore(
+  {
+    reducer: { ui: uiSlice.reducer, cart: cartSlice.reducer },
+  },
+  composeWithDevTools()
+);
 
 export default store;
