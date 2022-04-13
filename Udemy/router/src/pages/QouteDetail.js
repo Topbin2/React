576005e9a072fab6affec8d20f378986ce791739
SANGBIN <mosangbin94@gmail.com>
2022-04-13 13:39:05 +0900

    router

--- Udemy/router/src/pages/QouteDetail.js
+++ Udemy/router/src/pages/QouteDetail.js
@@ -1,16 +1,36 @@
 import React, { Fragment } from "react";
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 
-import Comments from '../components/comments/Comments';
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import Comments from "../components/comments/Comments";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "Sangbin", text: "Learning React is Great!" },
+];
 
 const QouteDetail = () => {
+  const match = useRouteMatch();
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
 
   return (
     <Fragment>
-      <h1>Qoute Detail Page</h1>
-      <p>{params.quoteId}</p>
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={match.path} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </Fragment>
