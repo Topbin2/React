0c040f8584a3cac3c1670fe5484f723bab382d82
SANGBIN <mosangbin94@gmail.com>
2022-04-14 21:02:30 +0900

    typescript

--- master-class/typescript/src/App.tsx
+++ master-class/typescript/src/App.tsx
@@ -1,33 +1,10 @@
 import React from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-interface DummyProps {
-  text: string;
-  otherThingHere?: boolean;
-}
-
-function Dummy({ text, otherThingHere }: DummyProps) {
-  return <h1>{text}</h1>;
-}
+import Router from "./Router";
 
 function App() {
-  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    console.log("clicl!");
-  };
 
   return (
-    <Container>
-      <Dummy text="sangbin" />
-        <button onClick={onClick}>btn</button>
-    </Container>
+    <Router />
   );
 }
 
