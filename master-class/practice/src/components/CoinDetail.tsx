d4d261eaee208b06d4e8d1a9acddf5e7a97bc6f4
SANGBIN <mosangbin94@gmail.com>
2022-04-18 09:27:54 +0900

    practice

--- master-class/practice/src/components/CoinDetail.tsx
+++ master-class/practice/src/components/CoinDetail.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 
@@ -5,18 +6,29 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
   h1 {
-    margin: 3.5rem 0;
+    margin: 3rem 0;
     font-size: 4rem;
     font-weight: 400;
     color: ${(props) => props.theme.accentColor};
   }
+
+  p {
+    font-weight: 400;
+  }
 `;
+
+const Description = styled.div`
+  width: 50%;
+  font-weight: 400;
+`
 
 const Card = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  margin: 1.5rem;
   padding: 1rem;
   width: 50%;
   height: 70px;
@@ -30,7 +42,7 @@ const Card = styled.div`
     flex-direction: column;
     align-items: center;
     p {
-      margin: 0.2rem;
+      margin: 0.5rem;
     }
   }
 `;
@@ -40,10 +52,52 @@ interface locationState {
   id: string;
 }
 
-const CoinDetail = () => {
-  const location = useLocation<locationState>();
+interface IInfo {
+  id:                 string;
+  name:               string;
+  symbol:             string;
+  rank:               number;
+  is_new:             boolean;
+  is_active:          boolean;
+  description:        string;
+  open_source:        boolean;
+}
 
+interface IPrice {
+  id:                 string;
+  name:               string;
+  symbol:             string;
+  rank:               number;
+  circulating_supply: number;
+  total_supply:       number;
+  max_supply:         number;
+  beta_value:         number;
+  first_data_at:      Date;
+  last_updated:       Date;
+}
+
+
+const CoinDetail = () => {
+  const [coinInfo, setCoinInfo] = useState<IInfo>();
+  const [coinPrice, setCoinPrice] = useState<IPrice>();
+  const location = useLocation<locationState>();
   const { state } = location;
+
+  useEffect(() => {
+    const fetchCoin = async () => {
+      const coinInfoResponse = await fetch(
+        `https://api.coinpaprika.com/v1/coins/${state.id}`
+      );
+      const coinInfoResult = await coinInfoResponse.json();
+
+      const coinPriceResponse = await fetch(`https://api.coinpaprika.com/v1/tickers/${state.id}`);
+      const coinPriceResult = await coinPriceResponse.json();
+      setCoinInfo(coinInfoResult);
+      setCoinPrice(coinPriceResult);
+    };
+    fetchCoin();
+  }, [state.id]);
+
 
   return (
     <Container>
@@ -51,15 +105,26 @@ const CoinDetail = () => {
       <Card>
         <div>
           <p>RANK:</p>
-          <p>1</p>
+          <p>{coinInfo?.rank}</p>
         </div>
         <div>
           <p>SYMBOL:</p>
-          <p>$BTC</p>
+          <p>{coinInfo?.symbol}</p>
         </div>
         <div>
           <p>OPEN SOURCE:</p>
-          <p>Yes</p>
+          <p>{coinInfo?.open_source ? 'Yes' : 'No'}</p>
+        </div>
+      </Card>
+      <Description>{coinInfo?.description}</Description>
+      <Card>
+        <div>
+          <p>TOTAL SUPLY:</p>
+          <p>{coinPrice?.total_supply}</p>
+        </div>
+        <div>
+          <p>MAX SUPPLY:</p>
+          <p>{coinPrice?.max_supply}</p>
         </div>
       </Card>
     </Container>
