ec5bf50b8f8d3b33d222f2f0f061661ebdc870fa
SANGBIN <mosangbin94@gmail.com>
2022-04-19 15:07:16 +0900

    add chart library

--- master-class/typescript/src/index.tsx
+++ master-class/typescript/src/index.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
@@ -7,13 +6,12 @@ import { theme } from "./theme";
 
 const queryClient = new QueryClient();
 
-ReactDOM.render(
-  <React.StrictMode>
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
     </QueryClientProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
 );
--- master-class/typescript/src/routes/Chart.tsx
+++ master-class/typescript/src/routes/Chart.tsx
@@ -1,31 +1,70 @@
 import { useQuery } from "react-query";
+import ReactApexChart from "react-apexcharts";
+
 import { fetchCoinHistory } from "../api";
 
 interface ChartProps {
   coinId: string;
 }
 
-interface ChartHistory {
-  time_open:  Date;
+interface IHistorical {
+  time_open: Date;
   time_close: Date;
-  open:       number;
-  high:       number;
-  low:        number;
-  close:      number;
-  volume:     number;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
   market_cap: number;
 }
 
-
 const Chart = ({ coinId }: ChartProps) => {
-  const { data, isLoading } = useQuery<ChartHistory>(["ohlcv", coinId], () => fetchCoinHistory(coinId));
+  const { data, isLoading } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
 
-  console.log(data);
-  
   return (
-    <h1>
-      Chart
-    </h1>
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ReactApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => price.close),
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            grid: { show: false },
+            stroke: {
+              curve: "smooth",
+              width: 4,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: { show: false },
+            },
+          }}
+        />
+      )}
+    </div>
   );
 };
 
