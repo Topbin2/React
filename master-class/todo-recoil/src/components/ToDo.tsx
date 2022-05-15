2f190660ad729814c971ffc8e1ec2bafa1800ad1
SANGBIN <mosangbin94@gmail.com>
2022-05-15 13:02:53 +0900

    todo

--- master-class/todo-recoil/src/components/ToDo.tsx
+++ master-class/todo-recoil/src/components/ToDo.tsx
@@ -4,12 +4,19 @@ import { useSetRecoilState } from "recoil";
 import { toDoState } from '../atoms';
 import { ITodo } from "../atoms";
 
+
+
 const ToDo = ({ text, category, id }: ITodo) => {
   const setToDos = useSetRecoilState(toDoState);
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-
     const { currentTarget: { name } } = event;
-
+    setToDos(oldToDos => {
+      const targetIndex = oldToDos.findIndex(toDo => toDo.id === id);
+      const oldToDo = oldToDos[targetIndex];
+      const newToDo = { text, id, category: name };
+      console.log(oldToDo, newToDo);
+      return oldToDos;
+    })
   };
 
   return <li>
