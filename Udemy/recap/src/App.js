e537382dffa27b2cd8c9ecf2856653dfc3eb0a2a
MoSangBin <mosangbin94@gmail.com>
2022-05-29 20:47:54 +0900

    props

--- Udemy/recap/src/App.js
+++ Udemy/recap/src/App.js
@@ -1,5 +1,17 @@
+
+import Todo from "./components/Todo";
+
+
 function App() {
-  return <div className="App">Heelo!</div>;
+
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo title='react' />
+      <Todo title='javascript' />
+      <Todo title='css' />
+    </div>
+  );
 }
 
 export default App;
