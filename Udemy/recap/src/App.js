178996ebc6fa71ea653a89f21b1483793be59783
MoSangBin <mosangbin94@gmail.com>
2022-05-29 21:28:36 +0900

    route

--- Udemy/recap/src/App.js
+++ Udemy/recap/src/App.js
@@ -1,15 +1,23 @@
+import { Route, Switch } from "react-router-dom";
 
-import Todo from "./components/Todo";
-
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
 
 function App() {
-
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo title='react' />
-      <Todo title='javascript' />
-      <Todo title='css' />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- Udemy/recap/src/components/Backdrop.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Backdrop = (props) => {
-  return <div className="backdrop" onCancel={props.onClick} />;
-};
-
-export default Backdrop;
