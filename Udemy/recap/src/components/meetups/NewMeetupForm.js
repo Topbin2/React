d231f1ec0e649d2abe709e85412531eb01a729cc
MoSangBin <mosangbin94@gmail.com>
2022-05-31 23:33:36 +0900

    POST

--- Udemy/recap/src/components/meetups/NewMeetupForm.js
+++ Udemy/recap/src/components/meetups/NewMeetupForm.js
@@ -3,7 +3,7 @@ import { useRef } from 'react';
 import Card from '../ui/Card';
 import classes from './NewMeetupForm.module.css';
 
-const NewMeetupForm = () => {
+const NewMeetupForm = (props) => {
   const titleInputRef = useRef();
   const imageInputRef = useRef();
   const addressInputRef = useRef();
@@ -24,7 +24,7 @@ const NewMeetupForm = () => {
       description: enteredDescription,
     }
 
-    
+    props.onAddMeetup(meetupData);
   };
 
   return (
--- Udemy/recap/src/pages/NewMeetup.js
+++ Udemy/recap/src/pages/NewMeetup.js
@@ -1,10 +1,20 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
+  const addMeetupHandler = (meetupData) => {
+    fetch("https://react-http-9f6b5-default-rtdb.firebaseio.com/meetups.json", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    });
+  };
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 };
