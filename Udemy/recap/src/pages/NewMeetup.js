b306dca663bd91a9f7bdae648c24cf0912f196c1
MoSangBin <mosangbin94@gmail.com>
2022-05-31 22:58:33 +0900

    form

--- Udemy/recap/src/pages/NewMeetup.js
+++ Udemy/recap/src/pages/NewMeetup.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
   return (
-    <div>
-      New Meetup Page
-    </div>
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm />
+    </section>
   );
 };
 
