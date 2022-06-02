0317873c2f1b66499a2260029e1e2fa6c1589224
MoSangBin <mosangbin94@gmail.com>
2022-06-02 20:13:18 +0900

    state

---
+++ Udemy/recap-hooks/src/components/Ingredients/IngredientForm.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+import Card from "../UI/Card";
+import "./IngredientForm.css";
+
+const IngredientForm = React.memo((props) => {
+  const inputState = useState({ title: "", amount: "" });
+
+  console.log(inputState);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    // ...
+  };
+
+  return (
+    <section className="ingredient-form">
+      <Card>
+        <form onSubmit={submitHandler}>
+          <div className="form-control">
+            <label htmlFor="title">Name</label>
+            <input
+              type="text"
+              id="title"
+              value={inputState[0].title}
+              onChange={(e) => inputState[1](e.target.value)}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="amount">Amount</label>
+            <input
+              type="number"
+              id="amount"
+              value={inputState[0].amount}
+              onChange={(e) => inputState[1](e.target.amount)}
+            />
+          </div>
+          <div className="ingredient-form__actions">
+            <button type="submit">Add Ingredient</button>
+          </div>
+        </form>
+      </Card>
+    </section>
+  );
+});
+
+export default IngredientForm;
