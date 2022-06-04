1d29789e813ee4590b90f3b1773c2bec45e27395
MoSangBin <mosangbin94@gmail.com>
2022-06-05 00:45:34 +0900

    context

--- Udemy/recap-hooks/src/context/auth-context.js
+++ Udemy/recap-hooks/src/context/auth-context.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState, createContext } from "react";
 
-export const AuthContext = () =>
-  React.createContext({
+const AuthContext = () =>
+  createContext({
     isAuth: false,
     login: () => {},
   });
 
-const AuthContextProvider = ({ children }) => {
-  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+export const AuthContextProvider = (props) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const loginHandler = () => {
+    setIsAuthenticated(true);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ login: loginHandler, isAuth: isAuthenticated }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
 };
 
-export default AuthContextProvider;
+export default AuthContext;
