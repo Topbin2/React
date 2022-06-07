83d8083cb0c8c8e496bf3e39a2879fb8b2a359cc
MoSangBin <mosangbin94@gmail.com>
2022-06-07 23:03:43 +0900

    selector

--- master-class/trello/src/App.tsx
+++ master-class/trello/src/App.tsx
@@ -1,14 +1,19 @@
 import React from "react";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import "./App.css";
 
-import { minuteState } from "./atoms";
+import { minuteState, hourSelector } from "./atoms";
 
 function App() {
   const [minutes, setMinutes] = useRecoilState(minuteState);
+  const [hours, setHours] = useRecoilState(hourSelector);
 
   const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
     setMinutes(+event.currentTarget.value);
+  };
+
+  const onHoursChange = (event:React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
   };
 
   return (
@@ -19,7 +24,12 @@ function App() {
         placeholder="Minutes"
         onChange={onMinutesChange}
       />
-      <input type="number" placeholder="Hours" />
+      <input
+        type="number"
+        placeholder="Hours"
+        value={hours}
+        onChange={onHoursChange}
+      />
     </div>
   );
 }
